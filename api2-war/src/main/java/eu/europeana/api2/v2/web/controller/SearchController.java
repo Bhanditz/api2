725dfdd9de0a054fbaae3bd5a89ca1ef3481a555
Luthien-in-edhil <maike.dulk@europeana.eu>
2018-04-24 18:07:02 +0200

    try concatenating msg and stacktrace

--- api2-war/src/main/java/eu/europeana/api2/v2/web/controller/SearchController.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/SearchController.java
@@ -159,7 +159,7 @@ public class SearchController {
                 Integer wickie = null;
                 wickie.toString();
             } catch (NullPointerException npe) {
-                LOG.error("Grutjes", npe);
+                LOG.error("Grutjes! Moeinoukieke: " + Arrays.toString(npe.getStackTrace()));
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
                 return JsonUtils.toJson(new ApiError("", "Grutjes"), callback);
             }
